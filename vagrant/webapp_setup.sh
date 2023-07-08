bbf84b52507d169a454146eb033fdde6305155fa
Md. Atiqul Islam <atiqul.islam@bjitacademy.com>
2023-07-08 15:40:03 +0600

    Update lombok dependency

--- vagrant/webapp_setup.sh
+++ vagrant/webapp_setup.sh
@@ -3,5 +3,5 @@ dnf install git maven wget -y
 
 git clone -b manual-deployment https://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@github.com/iatiqul/lms-devops-final.git
 cd lms-devops-final
-./mvnw clean install
-./mvnw spring-boot:run
+mvn clean install
+mvn spring-boot:run
