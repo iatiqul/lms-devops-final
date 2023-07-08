12f7db001ef61f641e1f8f4d7149af9922d9c2f3
Md. Atiqul Islam <atiqul.islam@bjitacademy.com>
2023-07-08 12:20:51 +0600

    Initial commit

---
+++ src/main/java/com/spark/lms/SparkLmsApplication.java
@@ -0,0 +1,15 @@
+package com.spark.lms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class SparkLmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparkLmsApplication.class, args);
+	}
+	
+}
